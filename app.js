b5c02c17bcff2a848d387242527d321b9ea00c3c
Ajaz Ahmad Qureshi <ajaz.ahmad@powersoft19.com>
2023-08-01 20:56:07 +0500

    MongoDB added

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-
 const { MongoClient } = require("mongodb");
 
 class GraphService {
-  constructor(connectionString, dbName, collectionName) {
+  constructor(connectionString, dbName, nodeCollectionName, edgeCollectionName) {
     this.connectionString = connectionString;
     this.dbName = dbName;
-    this.collectionName = collectionName;
+    this.nodeCollectionName = nodeCollectionName;
+    this.edgeCollectionName = edgeCollectionName;
     this.client = new MongoClient(connectionString, { useUnifiedTopology: true });
   }
 
@@ -15,7 +15,8 @@ class GraphService {
       await this.client.connect();
       console.log("Connected to MongoDB");
       this.db = this.client.db(this.dbName);
-      this.collection = this.db.collection(this.collectionName);
+      this.nodeCollection = this.db.collection(this.nodeCollectionName);
+      this.edgeCollection = this.db.collection(this.edgeCollectionName);
     } catch (error) {
       console.error("Error connecting to MongoDB:", error);
     }
@@ -32,16 +33,48 @@ class GraphService {
 
   async saveNodes(nodes) {
     try {
-      await this.collection.insertMany(nodes);
+      await this.nodeCollection.insertMany(nodes);
       console.log("Nodes saved to MongoDB.");
     } catch (error) {
       console.error("Error saving nodes to MongoDB:", error);
     }
   }
 
+  async saveEdges(edges) {
+    try {
+      await this.edgeCollection.insertMany(edges);
+      console.log("Edges saved to MongoDB.");
+    } catch (error) {
+      console.error("Error saving edges to MongoDB:", error);
+    }
+  }
+  async saveNodesAndEdges(graph){
+    try {
+      const nodesToSave = Array.from(graph.nodes.values());
+      const edgesToSave = [];
+    
+      // Convert edges from the graph to a format suitable for saving in MongoDB
+      for (const node of graph.nodes.values()) {
+        for (const edge of node.edges) {
+          edgesToSave.push({
+            fromNodeId: edge.fromNodeId,
+            toNodeId: edge.toNodeId,
+            type: edge.type,
+            weight: edge.weight
+          });
+        }
+      }
+      await this.saveNodes(nodesToSave);
+      await this.saveEdges(edgesToSave);
+    
+
+    }catch(error){
+      console.log("Error saving edges to MongoDB",error);
+    }
+  }
   async loadNodes() {
     try {
-      const nodes = await this.collection.find().toArray();
+      const nodes = await this.nodeCollection.find().toArray();
       console.log("Nodes loaded from MongoDB.");
       return nodes;
     } catch (error) {
@@ -49,15 +82,74 @@ class GraphService {
       return [];
     }
   }
-  loadGraphFromNodes(loadedNodes) {
+  async loadEdges() {
+    try {
+      const edges = await this.edgeCollection.find().toArray();
+      console.log("Edges loaded from MongoDB.");
+      return edges;
+    } catch (error) {
+      console.error("Error loading edges from MongoDB:", error);
+      return [];
+    }
+  }
+  loadGraphFromNodes(loadedNodes, loadedEdges) {
+    const graph = new Graph();
+    const nodesMap = new Map();
+
+    // Create nodes with edges information
+    loadedNodes.forEach(({ nodeId, type, data }) => {
+      graph.addNode(nodeId, type, data);
+      nodesMap.set(nodeId, graph.nodes.get(nodeId));
+    });
+
+    // Add edges to the graph
+    loadedEdges.forEach(({ fromNodeId, toNodeId, type, weight }) => {
+      const node = nodesMap.get(fromNodeId);
+      if (node) {
+        graph.addEdge(fromNodeId, toNodeId, type, weight);
+      }
+    });
+
+    return graph;
+  }
+
+  async loadGraph() {
+    try {
+      const loadedNodes = await this.nodeCollection.find().toArray();
+      const loadedEdges = await this.edgeCollection.find().toArray();
+      console.log("Nodes and edges loaded from MongoDB.");
+      return this.loadGraphFromNodes(loadedNodes, loadedEdges);
+    } catch (error) {
+      console.error("Error loading graph from MongoDB:", error);
+      return null;
+    }
+  }
+  loadSampleData() {
     const graph = new Graph();
 
-    loadedNodes.forEach(({ nodeId, type, data, edges }) => {
-      graph.addNode(nodeId, type, data);
-      edges.forEach((neighborId) => {
-        graph.addEdge(nodeId, neighborId);
-      });
-    });
+    // Sample data for nodes
+    const projectData = { title: "Sample Project", description: "This is a sample project" };
+    const taskData = { title: "Sample Task", description: "This is a sample task" };
+    const subTaskData = { title: "Sample Sub Task", description: "This is a sample sub-task" };
+    const discussionData = { content: "This is a sample discussion" };
+    const userData1 = { username: "user1", role: "developer" };
+    const userData2 = { username: "user2", role: "manager" };
+
+    // Add nodes to the graph
+    graph.addNode("project1", NODE_TYPE.PROJECT, projectData);
+    graph.addNode("task1", NODE_TYPE.TASK, taskData);
+    graph.addNode("subtask1", NODE_TYPE.SUB_TASK, subTaskData);
+    graph.addNode("user1", NODE_TYPE.USER, userData1);
+    graph.addNode("user2", NODE_TYPE.USER, userData2);
+
+    // Add edges between nodes
+    graph.addEdge("project1", "task1", EDGE_TYPE.DIRECTED, EDGE_CATEGORY.ASSIGN);
+    graph.addEdge("task1", "subtask1", EDGE_TYPE.DIRECTED, EDGE_CATEGORY.DEPENDENCY);
+
+    // Add discussions to nodes and attach users to discussions
+    const discussionNodeId1 = graph.addDiscussionToNode("task1", discussionData);
+    graph.addEdge(discussionNodeId1, "user1", EDGE_TYPE.UNDIRECTED, EDGE_CATEGORY.RELATED);
+    graph.addEdge(discussionNodeId1, "user2", EDGE_TYPE.UNDIRECTED, EDGE_CATEGORY.RELATED);
 
     return graph;
   }
@@ -69,13 +161,32 @@ const NODE_TYPE = {
     USER: "user",
     DISCUSSION: "discussion"
   };
+  const EDGE_TYPE = {
+    DIRECTED: "directed",
+    UNDIRECTED: "undirected"
+  };
+  
+  const EDGE_CATEGORY = {
+    ASSIGN: "assign",
+    DEPENDENCY: "dependency",
+    RELATED: "related"
+  };
+  
+  class Edge {
+    constructor(fromNodeId, toNodeId, type, category, weight = 1) {
+      this.fromNodeId = fromNodeId;
+      this.toNodeId = toNodeId;
+      this.type = type;
+      this.category = category;
+      this.weight = weight;
+    }
+  }
   class Node {
     constructor(nodeId, type, data) {
       this.nodeId = nodeId;
       this.type = type;
       this.data = data;
-      this.edges = []; // Array to store outgoing edges (direction)
-      this.reverseEdges = []; // Array to store incoming edges (reverse direction)
+      this.edges = [];
     }
   }
   
@@ -91,38 +202,38 @@ const NODE_TYPE = {
       }
     }
   
-    // Add directed edge between two nodes
-    addEdge(fromNodeId, toNodeId) {
-      if (this.nodes.has(fromNodeId) && this.nodes.has(toNodeId)) {
-        this.nodes.get(fromNodeId).edges.push(toNodeId);
-        this.nodes.get(toNodeId).reverseEdges.push(fromNodeId);
-      }
+  // Add directed edge between two nodes
+
+  addEdge(fromNodeId, toNodeId, edgeType, edgeCategory, weight = 1) {
+    if (this.nodes.has(fromNodeId) && this.nodes.has(toNodeId)) {
+      const edge = new Edge(fromNodeId, toNodeId, edgeType, edgeCategory, weight);
+      this.nodes.get(fromNodeId).edges.push(edge);
+
+      // if (edgeType === EDGE_TYPE.UNDIRECTED) {
+      //   const reverseEdge = new Edge(toNodeId, fromNodeId, edgeType, edgeCategory, weight);
+      //   this.nodes.get(toNodeId).edges.push(reverseEdge);
+      // }
+    }
+  }
+  addDiscussionToNode(parentNodeId, discussionData) {
+    if (!this.nodes.has(parentNodeId)) {
+      return null; // Parent node not found, cannot add discussion
     }
 
-    addDiscussionToNode(nodeId, author, content, userNodeId) {
-        if (!this.nodes.has(nodeId)) {
-          console.log("Node not found.");
-          return;
-        }
-    
-        const randomValue = Math.floor(Math.random() * 1000); // Generate a random value
-        const discussionId = `discussion_${Date.now()}_${randomValue}`; // Create a unique ID for each discussion
-        const discussionData = {
-          author: author,
-          timestamp: new Date(),
-          content: content
-        };
-    
-        const discussionNode = new Node(discussionId, NODE_TYPE.DISCUSSION, discussionData);
-        this.nodes.set(discussionId, discussionNode); // Add discussion node to graph
-    
-        this.addEdge(nodeId, discussionId); // Add edge between the node and the discussion
-    
-        // If userNodeId is provided, create an edge between User node and Discussion node
-        if (userNodeId && this.nodes.has(userNodeId)) {
-          this.addEdge(userNodeId, discussionId);
-        }
-      }
+    const discussionNodeId = this.generateUniqueId();
+    const discussionNode = new Node(discussionNodeId, NODE_TYPE.DISCUSSION, discussionData);
+    this.nodes.set(discussionNodeId, discussionNode);
+
+    // Add an "related" undirected edge between parent node and discussion node
+    this.addEdge(parentNodeId, discussionNodeId, EDGE_TYPE.UNDIRECTED, EDGE_CATEGORY.RELATED);
+
+    return discussionNodeId;
+  }
+
+  generateUniqueId() {
+    // Generate a unique ID for a node (you can use any method to generate unique IDs)
+    return "node_" + Math.random().toString(36).substr(2, 9);
+  }
 
     findNodesByTypeUnderParent(parentNodeId, targetType) {
       const visited = new Set();
@@ -155,38 +266,39 @@ const NODE_TYPE = {
         }
       }
 
-      findDiscussionsByUserUnderParent(parentNodeId, userNodeId) {
+      findDiscussionsByUserUnderParent(parentNodeId, userId) {
         const discussions = [];
-        const visited = new Set();
     
-        this.dfsFindDiscussions(parentNodeId, userNodeId, visited, discussions);
+        // Perform depth-first search (DFS) to find discussions connected to the user
+        this.dfsFindDiscussions(parentNodeId, userId, discussions);
     
         return discussions;
       }
     
-      dfsFindDiscussions(nodeId, userNodeId, visited, discussions) {
+      dfsFindDiscussions(nodeId, userId, discussions, visited = new Set()) {
         if (visited.has(nodeId)) {
           return;
         }
     
         visited.add(nodeId);
+        const node = this.nodes.get(nodeId);
     
-        if (!this.nodes.has(nodeId)) {
-          return;
-        }
-    
-        if (this.nodes.get(nodeId).type === NODE_TYPE.DISCUSSION) {
-          if (this.nodes.get(nodeId).reverseEdges.includes(userNodeId)) {
-            discussions.push(this.nodes.get(nodeId));
+        // Check if the node is a discussion and connected to the user
+        if (node.type === NODE_TYPE.DISCUSSION) {
+          for (const edge of node.edges) {
+            const userNode = this.nodes.get(edge.toNodeId);
+            if (userNode.nodeId === userId) {
+              discussions.push(node);
+              break; // No need to check further for this discussion
+            }
           }
         }
     
-        const edges = this.nodes.get(nodeId).edges;
-        for (const neighborId of edges) {
-          this.dfsFindDiscussions(neighborId, userNodeId, visited, discussions);
+        // Continue DFS for child nodes
+        for (const edge of node.edges) {
+          this.dfsFindDiscussions(edge.toNodeId, userId, discussions, visited);
         }
       }
-
   }    function printNodesArray(nodesArray) {
         console.log("Nodes Array:");
         for (const node of nodesArray) {
@@ -199,72 +311,27 @@ const NODE_TYPE = {
         }
       }
       
-  // Example usage:
-  //const graph = new Graph();
 
-//   graph.addNode("project1", NODE_TYPE.PROJECT, { name: "Project A", description: "This is project A" });
-//   graph.addNode("project2", NODE_TYPE.PROJECT, { name: "Project B", description: "This is project B" });
-//   graph.addNode("task1", NODE_TYPE.TASK, { description: "Task 1" });
-//   graph.addNode("task2", NODE_TYPE.TASK, { description: "Task 2" });
-//   graph.addNode("user1", NODE_TYPE.USER, { name: "John Doe", email: "john@example.com" });
-//   graph.addNode("user1", NODE_TYPE.USER, { name: "Jane Smith", email: "jane@example.com" });
-
-//   // Add a sample sub task under "task1"
-//   graph.addNode("subTask1", NODE_TYPE.SUB_TASK, { description: "Sub Task 1", parentTask: "task1" });
-//   graph.addNode("subTask2", NODE_TYPE.SUB_TASK, { description: "Sub Task 2", parentTask: "task1" });
-  
-//   // Add some sample sub tasks under "task2"
-//   graph.addNode("subTask3", NODE_TYPE.SUB_TASK, { description: "Sub Task 3", parentTask: "task2" });
-//   graph.addNode("subTask4", NODE_TYPE.SUB_TASK, { description: "Sub Task 4", parentTask: "task2" });
-  
-//   //graph.addEdge("project1", "project2");
-//   graph.addEdge("project1", "task1");
-//   graph.addEdge("project1", "task2");
-//   graph.addEdge("task1", "user1");
-  
-//   // Add edges between task1 and its sub tasks
-//   graph.addEdge("task1", "subTask1");
-//   graph.addEdge("task1", "subTask2");
-  
-//   // Add edges between task2 and its sub tasks
-//   graph.addEdge("task2", "subTask3");
-//   graph.addEdge("task2", "subTask4");
-  
-
-// const projectsUnderProject1 = graph.findNodesByTypeUnderParent("project1", NODE_TYPE.SUB_TASK);
-// //console.log("Sub tasks under project1:", projectsUnderProject1);
-// //printNodesArray(projectsUnderProject1);
-
-// graph.addDiscussionToNode("subTask1", "John Doe", "Discussing progress...","user1");
-// graph.addDiscussionToNode("subTask1", "Jane Smith", "Need more information...","user2");
-
-// // Add discussions to "project1"
-// graph.addDiscussionToNode("project1", "Alice", "Project status update...","user1");
-// graph.addDiscussionToNode("project1", "Bob", "Budget discussion...","user2");
-
-// // Find all discussions under "subTask1"
-// const discussionsUnderSubTask1 = graph.findNodesByTypeUnderParent("subTask1", NODE_TYPE.DISCUSSION);
-// //printNodesArray(discussionsUnderSubTask1);
-// //  console.log(graph.getNodes());
-// const discussionsUnderSubTask1ForUser1 = graph.findDiscussionsByUserUnderParent("project1", "user1");
-// printNodesArray(discussionsUnderSubTask1ForUser1);
-// //console.log("Discussions under subTask1 connected to user1:", discussionsUnderSubTask1ForUser1);  
-
-
-const graphService = new GraphService("mongodb://localhost:27017", "graphdb-v2", "nodes");
-
+const graphService = new GraphService(
+  "mongodb://localhost:27017",
+  "graphdb-v2",
+  "nodes",
+  "edges"
+);
 (async () => {
   await graphService.connect();
-
+  //const graph = graphService.loadSampleData();
   // ... (Add nodes and edges to the graph as shown in previous examples)
-
+ 
+  const loadedNodes = await graphService.loadNodes();
+  const loadedEdges = await graphService.loadEdges();
+  const graph = graphService.loadGraphFromNodes(loadedNodes,loadedEdges);
   // Save nodes to MongoDB
-  //const nodesToSave = Array.from(graph.nodes.values());
-  //await graphService.saveNodes(nodesToSave);
+  //await graphService.saveNodesAndEdges(graph);
 
   // Load nodes from MongoDB
-  const loadedNodes = await graphService.loadNodes();
-  const graph = graphService.loadGraphFromNodes(loadedNodes);
+  //const loadedNodes = await graphService.loadNodes();
+  //const graph = graphService.loadGraphFromNodes(loadedNodes);
   
   const discussionsUnderSubTask1ForUser1 = graph.findDiscussionsByUserUnderParent("project1", "user1");
   printNodesArray(discussionsUnderSubTask1ForUser1);
